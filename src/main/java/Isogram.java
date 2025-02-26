550639508284f533e30bb1558d7518f6671f0b3f
Samuel Crain <samuelbcrain@gmail.com>
2025-02-26 01:44:50 +0000

    Coded method to find if given string is Isogram.

--- src/main/java/Isogram.java
+++ src/main/java/Isogram.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+
+import com.fasterxml.jackson.databind.node.NumericNode;
 
 public class Isogram {
     /**
@@ -13,6 +16,14 @@ public class Isogram {
      * @return true if str is an isogram, false otherwise.
      */
     public boolean isIsogram(String str){
-        return false;
+        boolean result = true;
+        HashSet<Character> characters = new HashSet<>();
+        for(int i = 0; i < str.length(); i++) {
+            result = characters.add(str.charAt(i));
+            if (!result) {
+                break;
+            }
+        }
+        return result;
     }
 }
